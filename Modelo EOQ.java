b35fcbbbc976b4f174f630d959bde0bd517f0efb
marquitomaldad <marquitomaldad@gmail.com>
2025-11-13 10:47:43 -0600

    Ingreso de Datos

--- Modelo EOQ.java
+++ Modelo EOQ.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner lector = new Scanner(System.in);
         System.out.println("Modelo EOQ Clásico");
     }
 
