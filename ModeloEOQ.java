aa6eb50df455de29ab2f0d31d3c38690862b62f3
marquitomaldad <marquitomaldad@gmail.com>
2025-11-13 18:31:49 -0600

    Modelo EOQ Clásico

--- ModeloEOQ.java
+++ ModeloEOQ.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ModeloEOQ {
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
-
+        
         int opcion;
 
         do{
@@ -14,7 +14,27 @@ public class ModeloEOQ {
             System.out.println("Elige una opción");
             opcion =lector.nextInt();
 
-        }
+            if (opcion ==1){
+                double D,S,H;
+
+                System.out.println("Modelo EOQ Clásico");
+
+                System.out.println("Demanda Anual (D)");
+                D = lector.nextDouble();
+
+                System.out.println("Costo de Pedido (S)");
+                S = lector.nextDouble();
+
+                System.out.println("Costo de Mantenimiento (H)");
+                H = lector.nextDouble();
+
+                double eoq = Math.sqrt((2 * D * S) / H);
+
+                System.out.println("El EOQ es: " + eoq);
+                System.out.println();
+            }
+
+        } while (opcion !=3);
 
     }
 
