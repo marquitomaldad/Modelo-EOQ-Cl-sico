e293e5a3221abc841cb18fd169e5f853c464168f
marquitomaldad <marquitomaldad@gmail.com>
2025-11-13 18:11:00 -0600

    Menú

--- ModeloEOQ.java
+++ ModeloEOQ.java
@@ -2,9 +2,20 @@ import java.util.Scanner;
 
 public class ModeloEOQ {
     public static void main(String[] args) {
-
         Scanner lector = new Scanner(System.in);
-        System.out.println("Modelo EOQ Clásico y con Descuentos");
+
+        int opcion;
+
+        do{
+            System.out.println("-Menú de modelos EOQ-");
+            System.out.println("1. EOQ Clásico");
+            System.out.println("2. EOQ con Descuentos");
+            System.out.println("3. Salir");
+            System.out.println("Elige una opción");
+            opcion =lector.nextInt();
+
+        }
+
     }
 
 }
